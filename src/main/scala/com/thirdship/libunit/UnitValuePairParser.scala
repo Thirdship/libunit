7bac318395d26ef3b0f627c71e7c8de485f8cc74
John Starich <john.starich@thirdship.com>
2016-01-29 20:46:25 -0600

    Add measure regex

--- src/main/scala/com/thirdship/libunit/UnitValuePairParser.scala
+++ src/main/scala/com/thirdship/libunit/UnitValuePairParser.scala
@@ -6,7 +6,36 @@ import scala.util.matching.Regex
   * Created by jacobingalls on 1/28/16.
   */
 object UnitValuePairParser {
-	private final val reg = new Regex("(-*[0-9]*\\.?[0-9]+)\\s*(.*)?", "number", "unit")
+	private final val reg = Pattern.compile("""(?x) # x ignores whitespace and comments
+		  |(?<number>                       # the entire number
+	      |  (?<sign>[\+\-])?               # the sign of the number
+	      |  (?:                            # the absolute value of the number
+	      |    (?<hex> 0x[0-9a-fA-F]+ )     # hex form of a number
+	      |    | (?<binary>0[bB][01]+)      # binary form of a number
+	      |    | (?:                        # standard decimal form of a number
+	      |      (?<integer>                # the whole integer part of the number
+	      |        (?:
+	      |          [0]*                   # leading zeroes
+	      |          [1-9]                  # a non-zero
+	      |          [0-9]{0,2}             # up to two other numbers in first '000,'
+	      |          (?:\,[0-9]{3})+        # more consecutive terms of ',000'
+	      |        )
+	      |        | [0-9]+                 # or just one big number
+	      |      )?
+	      |      \.?
+	      |      (?<decimal>                # the fractional digits in this floating point number
+	      |        (?<=\.)[0-9]+
+	      |      )?
+	      |      (?:                        # exponent for this base 10 number
+	      |        e(?<exponent> [\+\-]?[0-9]+ )
+	      |      )?
+	      |    )
+	      |  )
+	      |)
+	      |(?:                              # units indicating what the number means
+	      |  \s* (?<unit>[a-zA-Z'\/"]+)
+	      |)?
+	      |""".stripMargin)
 
 	def apply(str: String) = parse(str)
 
